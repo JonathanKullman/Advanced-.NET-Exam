ef2b25f76e59a25b53c35aaff08554148b8259fb
Jonathan Kullman <jontekulljobb@gmail.com>
2021-04-05 12:49:46 +0200

    Fixed Owner prop

--- Tenta-Advnet-Jonathan-Kullman-2/Data/Owner.cs
+++ Tenta-Advnet-Jonathan-Kullman-2/Data/Owner.cs
@@ -5,8 +5,7 @@ namespace Tenta_Advnet_Jonathan_Kullman_2
     public class Owner
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; }
         public virtual ICollection<Hamster> Hamsters { get; set; } 
     }
 }
